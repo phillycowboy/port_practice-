a155370b6bd30facbbca764d10edba99c0e01f26
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-08-29 14:06:32 -0400

    trying to solve problem with multiple cards for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,12 +3,17 @@ import React, {Component} from 'react'
 class Projects extends Component{
     render(){
         return(
-            <div id="projects">
-                <h1>PROJECTS</h1>
-                <h5>make a card for each projects
-                    name and link to github and youtube
-                    add hover so that it flips and tells you languages
-                </h5>
+            <div>
+                <div id="projects">
+                    <h1>First Responder</h1>
+                    {/* <h5>make a card for each projects
+                        name and link to github and youtube
+                        add hover so that it flips and tells you languages
+                    </h5> */}
+                    <div id="projects">
+                        <h1>Tasker</h1>
+                    </div>
+                </div>
             </div>
         )
     }
